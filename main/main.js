b75bf845fa7670b6eca93d48f10abc70aea23abe
alichoriyev2004 <alichoriyev2004@gmail.com>
2025-10-20 01:15:38 +0500

    7

--- main/main.js
+++ main/main.js
@@ -5,3 +5,27 @@ const box = document.getElementById("myBox");
 closeBtn.addEventListener("click", () => {
   box.style.display = "none";
 });
+
+      function openPDF() {
+        const correctCode = "4148";
+        const userInput = document.getElementById("codeInput").value.trim();
+
+        if (userInput === correctCode) {
+          // Открытие PDF-файла в новой вкладке
+          window.open(".././pdf/касаллик варакаси Самандар.docx");
+        } else {
+          alert("Неверный код. Пожалуйста, попробуйте снова.");
+        }
+      }
+ 
+      const input = document.getElementById('codeInput');
+
+input.addEventListener('input', function() {
+  // faqat raqam bo‘lishi uchun
+  this.value = this.value.replace(/\D/g, '');
+
+  // maksimal uzunlik 4 ta
+  if (this.value.length > 4) {
+    this.value = this.value.slice(0, 4);
+  }
+});
