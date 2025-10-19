217f075dbdbb26eda51a14eddb34be11d3d33c58
alichoriyev2004 <alichoriyev2004@gmail.com>
2025-10-20 02:13:45 +0500

    8

--- main/main.js
+++ main/main.js
@@ -29,3 +29,54 @@ input.addEventListener('input', function() {
     this.value = this.value.slice(0, 4);
   }
 });
+
+
+const translations = {
+  uz: {
+    title: "Bosh sahifa",
+    desc: "Kirish",
+    btn: "Ochish",
+    infoTitle: "Ushbu hujjatdan foydalanish uchun siz avtorizatsiyadan o’tishingiz yoki, PIN kodini kiritishingiz lozim.",
+    infoText: "PIN-kod hujjatning QR-kodi yonida joylashgan",
+    footer: "Avtorizatsiyadan o’tish uchun bu yerga bosing"
+  },
+  ru: {
+    title: "Главная",
+    desc: "Войти",
+    btn: "Открыть",
+    infoTitle: "Пожалуйста пройдите авторизацию чтобы система определила вас. Если документ принадлежит вам, введения ПИН кода не требуется.",
+    infoText: "PIN-код размещается рядом с QR-кодом документа",
+    footer: "Нажмите здесь для авторизации"
+  },
+  en: {
+    title: "Бош саҳифа",
+    desc: "Кириш",
+    btn: "Очиш",
+    infoTitle: "Ушбу ҳужжатдан фойдаланиш учун сиз авторизатсиядан ўтишингиз ёки, ПИН кодини киритишингиз лозим.",
+    infoText: "ПИН код хужжатнинг QR коди ёнига жойлаштирилган",
+    footer: "Авторизатсиядан ўтиш  учун бу ерга босинг"
+  }
+};
+
+const langSelect = document.getElementById("lang");
+const elements = document.querySelectorAll("[data-key]");
+
+function changeLanguage(lang) {
+  elements.forEach(el => {
+    const key = el.getAttribute("data-key");
+    el.textContent = translations[lang][key];
+  });
+  localStorage.setItem("lang", lang);
+}
+
+// Yuklanishda saqlangan tilni o‘qish
+window.addEventListener("load", () => {
+  const savedLang = localStorage.getItem("lang") || "uz";
+  langSelect.value = savedLang;
+  changeLanguage(savedLang);
+});
+
+// Tanlanganda tilni o‘zgartirish
+langSelect.addEventListener("change", (e) => {
+  changeLanguage(e.target.value);
+});
